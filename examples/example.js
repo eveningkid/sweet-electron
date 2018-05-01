4cdc6ea030f999e24250303a8e27b0e4ab7844e4
eveningkid <arn.dellinger@gmail.com>
2018-05-01 11:23:46 +0800

    Modified example

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 const sweet = require('../src')(electron);
 
 sweet()
-  .url((is) => [__dirname, is.dev() ? 'index_dev.html' : 'index.html'])
+  .url(is => [__dirname, is.dev() ? 'index_dev.html' : 'index.html'])
   .window({ height: 800 })
-  .ready((is, mainWindow) => console.log(is.dev() ? 'dev' : 'prod'))
+  .ready(is => console.log('Mode in', is.dev() ? 'dev' : 'prod'))
   .run();
